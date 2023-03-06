ebfd970ac09fdb094d461b453ecc54ec91e5c44b
Rebecca Ribas <beky.ribas@gmail.com>
2023-03-05 23:15:43 -0500

    nit

--- ci.sh
+++ ci.sh
@@ -1,20 +1,6 @@
 #!/bin/bash -eu
 
-repo_dir=$PWD
-
-pkgs=$repo_dir/build/external-packages
-rm -rf $pkgs
-mkdir -p $pkgs
-
-tmp_dir=$(mktemp -d)
-pushd $tmp_dir
-
-curl -L -o bee.tar.gz https://github.com/bec-ca/bee/archive/refs/tags/v1.0.0.tar.gz
-tar -xf bee.tar.gz
-
-mv */bee $pkgs/
-
-popd
+./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.0.0.tar.gz
 
 for file in command/*.cpp; do
   echo "Compiling $file..."
