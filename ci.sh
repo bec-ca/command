b0f2a4fd51db659a90958777519c529b56c1d790
Rebecca Ribas <beky.ribas@gmail.com>
2023-03-11 16:39:36 -0500

    bump bee version

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.0.0.tar.gz
+./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.1.0.tar.gz
 
 for file in command/*.cpp; do
   echo "Compiling $file..."
